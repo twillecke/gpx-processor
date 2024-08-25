aa7ffe1b93ee71d730be6585c4c5c186273a4a97
Thiago Willecke <thiagogwillecke@gmail.com>
2024-08-25 10:08:10 -0300

    refactor: create apiCOntrollerDependencies objecy

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(compression());
 app.use(cors());
-const uploadMiddleware = multer();
-const trackRepository = new TrackRepositoryMemory();
-new APIController(app, { trackRepository, uploadMiddleware });
+const apiControllerDependencies = {
+	uploadMiddleware: multer(),
+	trackRepository: new TrackRepositoryMemory(),
+};
+new APIController(app, apiControllerDependencies);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on http://localhost:${process.env.PORT}`);
