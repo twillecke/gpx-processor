11ed4ef680e6c0d79300e38942e206cacff4229d
Thiago Willecke <thiagogwillecke@gmail.com>
2024-09-05 13:34:29 -0300

    feat: throw error if email already exists in user sign up

--- test/UseCase/UserSignUp.test.ts
+++ test/UseCase/UserSignUp.test.ts
@@ -8,11 +8,24 @@ test("Should sign up a user", async () => {
 		password: "1234",
 	};
 	const userSignUp = new UserSignUp(userRepositoryMemory);
-
 	const userSignUpOutput = await userSignUp.execute(input);
 	const storedUser = await userRepositoryMemory.getUserById(userSignUpOutput);
 
 	expect(userSignUpOutput).toBe(storedUser.userId);
 	expect(storedUser.email).toBe(input.email);
-	console.log("storedUser", storedUser);
+	console.log("repositoryState", userRepositoryMemory.users);
+});
+
+test("Should not sign up a user with the same email", async () => {
+	const userRepositoryMemory = new UserRepositoryMemory();
+	const input = {
+		email: "john.doe",
+		password: "1234",
+	};
+	const userSignUp = new UserSignUp(userRepositoryMemory);
+	const userSignUpOutput = await userSignUp.execute(input);
+
+	await expect(async () => await userSignUp.execute(input)).rejects.toThrow(
+		new Error("Email address already exists"),
+	);
 });
