45587cdac536429c1819f9341732da747e2a0286
Thiago Willecke <thiagogwillecke@gmail.com>
2024-10-10 16:20:23 -0300

    refactor: remove comment

--- src/UseCase/UserSaveNewTrack.ts
+++ src/UseCase/UserSaveNewTrack.ts
@@ -7,7 +7,6 @@ export default class UserSaveNewTrack {
 		this.trackRepository = trackRepository;
 	}
 	async execute(input: Input): Promise<string> {
-		// TODO: in future get authorId from a JWT token to avoid spoofing
 		const track = await Track.create({ metadata: input.metadata, trackData: input.trackData });
 		const savedTrackId = this.trackRepository.saveTrack(track);
 		return savedTrackId;
